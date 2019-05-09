3aa0944616076359ab7fe3b0b25319cc7135872b
yp <473075889>
2019-05-09 19:01:59 +0800

    Initial commit

---
+++ src/main/java/com/study/firstnetty/TestServer.java
@@ -0,0 +1,12 @@
+package com.study.firstnetty;
+
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+
+public class TestServer {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+    }
+}
---
+++ src/main/java/com/study/firstnetty/firstnetty.java
@@ -0,0 +1,5 @@
+package com.study.firstnetty;
+
+public class firstnetty {
+
+}
